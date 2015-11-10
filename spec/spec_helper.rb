6daa621cc739b56059b05923c84a636aa374fc0c
Matt Ewell <matt.ewell@gmail.com>
2015-11-10 14:35:50 -0700

    Add support for rails route helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,13 +9,15 @@ end
 RSpec.configure do |config|
   config.backtrace_exclusion_patterns = []
   config.backtrace_exclusion_patterns << /.*\/gems\/.*/
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
+
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
-
+  
   config.before(:suite) do
     DatabaseCleaner.start
     DatabaseCleaner.strategy = :transaction
