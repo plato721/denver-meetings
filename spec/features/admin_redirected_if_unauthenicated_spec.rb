b4a1f919e75273c9bddc2860bd0efcac5e9e2271
Matt Ewell <matt.ewell@gmail.com>
2015-11-09 21:02:19 -0700

    Remove rennamed test from staging

--- spec/features/admin_redirected_if_unauthenicated_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.feature "Admin redirected if unauthenticated" do
-  scenario "when accessing /admin" do
-    visit "/admin"
-    expect(current_path).to eq("http://github")
-  end
-end
