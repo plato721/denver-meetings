d431c92739d374fed40b7c794b36075a0346717f
Matt Ewell <matt.ewell@gmail.com>
2015-11-09 18:04:26 -0700

    Add oauth routes to github

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :admin do
     root to: "meetings#index"
+    get "auth/github", as: "login"
+    get "auth/github/callback", to: "sessions#create"
   end
 end
