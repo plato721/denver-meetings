e4fc9ae57db788664187f39fe7a9d80be83d9c12
Matt Ewell <matt.ewell@gmail.com>
2015-11-10 17:16:37 -0700

    Update tests to look for new not index path for searching

---
+++ spec/controllers/mobile/search_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Mobile::SearchController, type: :controller do
+
+end
--- spec/features/search_from_landing_spec.rb
+++ spec/features/search_from_landing_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature "Search from landing" do
     scenario "it is directed to a search form" do
       visit root_path
       click_link_or_button "meetings-button"
-      expect(current_path).to eq(mobile_search_index_path)
+      expect(current_path).to eq(new_mobile_search_path)
     end
   end
 end
