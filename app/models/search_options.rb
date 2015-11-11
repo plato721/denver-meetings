dc76975b0118f3954dbf848ef9a981c4e2dffd31
Matt Ewell <matt.ewell@gmail.com>
2015-11-10 22:27:46 -0700

    Flesh out search options poro

--- app/models/search_options.rb
+++ app/models/search_options.rb
@@ -3,8 +3,34 @@ class SearchOptions
   end
 
   def cities
-    @cities ||= Meeting.pluck
+    @cities ||= Meeting.uniq.pluck(:city)
   end
 
+  def times
+    @select_times ||= begin
+      times = Meeting.joins(:raw_meeting).uniq.pluck(:time).sort
+      times_am = times.select { |t| t =~ /A/ }
+      times_pm = times.select { |t| t =~ /P/ }
+      times_am.concat(times_pm)
+    end
+  end
+
+  def meeting_names
+    @names ||= Meeting.uniq.pluck(:group_name)
+  end
+
+  def days
+    @days ||= begin
+      ["Sunday", "Monday", "Tuesday", "Wednesday",
+      "Thursday", "Friday", "Saturday", "Sunday"]
+    end
+  end
+
+  def special
+    ["Beginner", "Women", "Men", "Gay", "Youth"]
+  end
+
+  def format
+  end
 
 end
