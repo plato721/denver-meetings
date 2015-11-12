7f699b722199dd2cbf219b89c492e6890a805c1f
Matt Ewell <matt.ewell@gmail.com>
2015-11-12 15:26:57 -0700

    Rename search_request to search

--- app/models/search_request.rb
+++
@@ -1,9 +0,0 @@
-class SearchRequest < ActiveRecord::Base
-  def self.create_request(params)
-    self.create({text: params["free_search"],
-              city: params["city"],
-              name: params["name"],
-              time: params["time"],
-              open: params["open"]})
-  end
-end
