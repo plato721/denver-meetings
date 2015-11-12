30cdc82126bf06ca8bbc913cb92045c39b03e4ef
Matt Ewell <matt.ewell@gmail.com>
2015-11-12 09:19:56 -0700

    Add simplecov rails command to get rails-style coverage report

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,13 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
-# Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+
 require 'spec_helper'
 require 'rspec/rails'
+require 'simplecov'
+
+SimpleCov.start "rails"
+
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
