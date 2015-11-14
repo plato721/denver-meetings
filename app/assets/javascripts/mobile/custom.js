8be85f3c4daf6d278038bcc7181e0da805041410
Matt Ewell <matt.ewell@gmail.com>
2015-11-14 14:57:51 -0700

    Put city text as italics as well. Set up js to remove this when user starts typing

--- app/assets/javascripts/mobile/custom.js
+++ app/assets/javascripts/mobile/custom.js
@@ -2,13 +2,17 @@
 var main = function(){
 
   $(".city-text").on("click", function() {
-    if ($(this).val() == "(Optional search text)")
+    if ($(this).val() == "(Optional search text)"){
         $(this).val("")
+        $(this).removeClass("city-text-default")
+      }
   });
 
   $(".group-text").on("click", function() {
-    if ($(this).val() == "(Optional search text)")
+    if ($(this).val() == "(Optional search text)"){
         $(this).val("")
+        $(this).removeClass("group-text-default")
+      }
   });
 
 
