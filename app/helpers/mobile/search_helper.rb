b4a8330e55deab39c308ff918e6e965587b94074
Matt Ewell <matt.ewell@gmail.com>
2015-11-15 12:20:51 -0700

    Put day, time, and distance (if applicable) on same line.

--- app/helpers/mobile/search_helper.rb
+++ app/helpers/mobile/search_helper.rb
@@ -2,4 +2,13 @@ module Mobile::SearchHelper
   def distance_display?(divider)
     divider.include?("mile")
   end
+
+  def meeting_with_distance(meeting)
+    "#{meeting.day}, #{meeting.time}, #{pluralize(meeting.distance, "mile")}"
+
+  end
+
+  def meeting_without_distance(meeting)
+    "#{meeting.day}, #{meeting.time}"
+  end
 end
