b52e76d08e27e0ae890c2829acb9e3fe2ac65eec
Matt Ewell <matt.ewell@gmail.com>
2015-11-15 22:54:25 -0700

    Feature model test up to 84%

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,2 +1,73 @@
 class Feature < ActiveRecord::Base
+  validate :uses_only_permitted_features
+  validates :code, uniqueness: true
+  validates :name, uniqueness: true
+
+  def uses_only_permitted_features
+    if !Feature.permitted_features.keys.include?(code)
+      errors.add(:code,
+        "Must use code from #{Feature.permitted_features.keys}")
+    elsif !Feature.permitted_features.values.include?(name)
+      errors.add(:name,
+        "Must use feature name from #{Feature.permitted_features.values}")
+    else
+      true
+    end
+  end
+
+  def self.get_asl
+    self.find_or_create_by(code: "ASL") do |feature|
+      feature.name = "Sign Language Interpreter"
+    end
+  end
+
+  def self.get_accessible
+    self.find_or_create_by(code: "*") do |feature|
+      feature.name = "Accessible"
+    end
+  end
+
+  def self.get_non_smoking
+    self.find_or_create_by(code: "n") do |feature|
+      feature.name = "Non-Smoking"
+    end
+  end
+
+  def self.get_sitter
+    self.find_or_create_by(code: "Sit") do |feature|
+      feature.name = "Sitter"
+    end
+  end
+
+  def self.match_asl(codes)
+    codes =~ /.*ASL.*/
+  end
+
+  def self.match_accessible(codes)
+    codes =~ /.*\*.*/
+  end
+
+  def self.match_non_smoking(codes)
+    codes =~ /[^pr]n/
+  end
+
+  def self.match_sitter(codes)
+    codes =~ /.*Sit.*/
+  end
+
+  def self.get_features(codes)
+    features = []
+    features << get_asl if match_asl(codes)
+    features << get_accessible if match_accessible(codes)
+    features << get_non_smoking if match_non_smoking(codes)
+    features << get_sitter if match_sitter(codes)
+    features
+  end
+
+  def self.permitted_features
+    {"ASL" => "Sign Language Interpreter",
+      "*" => "Accessible",
+      "n" => "Non-Smoking",
+    "Sit" => "Sitter"}
+  end
 end
--- spec/models/feature_spec.rb
+++ spec/models/feature_spec.rb
@@ -1,5 +1,89 @@
 require 'rails_helper'
 
 RSpec.describe Feature, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include_context "codes"
+  let(:valid_features) do
+    {"ASL" => "Sign Language Interpreter",
+      "*" => "Accessible",
+      "n" => "Non-Smoking",
+    "Sit" => "Sitter"}
+  end
+
+  it "can be created with a valid feature" do
+    code_lang = valid_features.first
+    attributes = [:code, :name].zip(code_lang).to_h
+
+    count = Feature.count
+    Feature.create(attributes)
+
+    expect(Feature.count).to eq(count + 1)
+  end
+
+  it "allows only valid Features" do
+    bad_feature = Feature.new(code: "G", name: "German")
+
+    expect(bad_feature).to_not be_valid
+  end
+
+  context "does not allow duplicate" do
+    before do
+      @code_feature = valid_features.first
+      @attributes = [:code, :name].zip(@code_feature).to_h
+
+      Feature.create(@attributes)
+    end
+
+    it "code" do
+      attributes = {code: @code_feature.first,
+        name: valid_features["Sit"].last}
+
+      dup = Feature.new(attributes)
+
+      expect(dup).to_not be_valid
+    end
+
+    it "Feature" do
+      attributes = {code: valid_features["Sit"].first,
+        name: @code_feature.last}
+
+      dup = Feature.new(attributes)
+
+      expect(dup).to_not be_valid
+    end
+  end
+
+  context "parses from aggregate code" do
+
+    it "finds ASL" do
+      raw_code = "ASL"
+      features = Feature.get_features(raw_code)
+
+      expect(features.count).to eq(1)
+      expect(features.first.name).to eq("Sign Language Interpreter")
+    end
+
+    it "finds accessible" do
+      raw_code = "*nFrnSp"
+      features = Feature.get_features(raw_code)
+      features = features.map { |f| f.name }
+  
+      expect(features.count).to eq(2)
+      expect(features.include?("Accessible")).to be_truthy
+    end
+
+    xit "finds polish" do
+      raw_code = "Pol"
+      features = Feature.get_features(raw_code)
+
+      expect(features.first.name).to eq("Polish")
+    end
+
+    xit "finds with other codes" do
+      raw_code = "*nFrnSp"
+      features = Feature.get_Features(raw_code)
+
+      expect(features.first.name).to eq("French")
+    end
+
+  end
 end
