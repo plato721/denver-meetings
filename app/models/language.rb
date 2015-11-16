5363d794ad83edda60cc728a34401a66bfebdbeb
Matt Ewell <matt.ewell@gmail.com>
2015-11-15 22:24:32 -0700

    Language creation and parsing from raw codes

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,17 +1,49 @@
 class Language < ActiveRecord::Base
   validate :uses_only_permitted_languages
+  validates :code, uniqueness: true
+  validates :name, uniqueness: true
 
   def uses_only_permitted_languages
-    if !permitted_languages.keys.include?(code)
-      errors.add(:code, "Must use code from #{permitted_languages.keys}")
-    elsif !permitted_languages.values.include?(name)
-      errors.add(:name, "Must use language name from #{permitted_languages.values}")
+    if !Language.permitted_languages.keys.include?(code)
+      errors.add(:code,
+        "Must use code from #{Language.permitted_languages.keys}")
+    elsif !Language.permitted_languages.values.include?(name)
+      errors.add(:name,
+        "Must use language name from #{Language.permitted_languages.values}")
+    else
+      true
     end
   end
 
-  def permitted_languages
-    {Spn: "Spanish",
-     Frn: "French",
-     Pol: "Polish"}
+  def self.get_french
+    self.find_or_create_by(code: "Frn") do |language|
+      language.name = "French"
+    end
+  end
+
+  def self.get_polish
+    self.find_or_create_by(code: "Pol") do |language|
+      language.name = "Polish"
+    end
+  end
+
+  def self.get_spanish
+    self.find_or_create_by(code: "Spn") do |language|
+      language.name = "Spanish"
+    end
+  end
+
+  def self.get_languages(codes)
+    permitted_languages.each_with_object([]) do |language, results|
+      if codes.include?(language.first)
+        results << self.send("get_#{language.last.downcase}".to_sym)
+      end
+    end
+  end
+
+  def self.permitted_languages
+    {"Spn" => "Spanish",
+     "Frn" => "French",
+     "Pol" => "Polish"}
   end
 end
