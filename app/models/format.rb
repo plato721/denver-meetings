6be25a251da5b9076461a6d1a6ab08a4f8bc7961
Matt Ewell <matt.ewell@gmail.com>
2015-11-16 13:29:51 -0700

    Add Format and test

--- app/models/format.rb
+++ app/models/format.rb
@@ -1,2 +1,132 @@
 class Format < ActiveRecord::Base
+  validate :uses_only_permitted_formats
+  validates :code, uniqueness: true
+  validates :name, uniqueness: true
+
+  def uses_only_permitted_formats
+    if !Format.permitted_formats.keys.include?(code)
+      errors.add(:code,
+        "Must use code from #{Format.permitted_formats.keys}")
+    elsif !Format.permitted_formats.values.include?(name)
+      errors.add(:name,
+        "Must use format name from #{Format.permitted_formats.values}")
+    else
+      true
+    end
+  end
+
+  def self.get_open
+    self.find_or_create_by(code: "O") do |format|
+      format.name = "Open"
+    end
+  end
+
+  def self.match_open(codes)
+    codes.include?("O")
+  end
+
+  def self.get_closed
+    self.find_or_create_by(code: "C") do |format|
+      format.name = "Closed"
+    end
+  end
+
+  def self.match_closed(codes)
+    codes =~ /(.*C[^A].*)|(.*C$)/
+  end
+
+  def self.get_speaker
+    self.find_or_create_by(code: "SP") do |format|
+      format.name = "Speaker"
+    end
+  end
+
+  def self.match_speaker(codes)
+    codes.include?("SP")
+  end
+
+  def self.get_step
+    self.find_or_create_by(code: "ST") do |format|
+      format.name = "Step"
+    end
+  end
+
+  def self.match_step(codes)
+    codes.include?("ST")
+  end
+
+  def self.get_big_book
+    self.find_or_create_by(code: "BB") do |format|
+      format.name = "Big Book"
+    end
+  end
+
+  def self.match_big_book(codes)
+    codes.include?("BB")
+  end
+
+  def self.get_grapevine
+    self.find_or_create_by(code: "GV") do |format|
+      format.name = "Grapevine"
+    end
+  end
+
+  def self.match_grapevine(codes)
+    codes.include?("GV")
+  end
+
+  def self.get_traditions
+    self.find_or_create_by(code: "T") do |format|
+      format.name = "Traditions"
+    end
+  end
+
+  def self.match_traditions(codes)
+    codes =~ /(.*[^S]T.*)|(^T.*)/
+  end
+
+  def self.get_candlelight
+    self.find_or_create_by(code: "CA") do |format|
+      format.name = "Candlelight"
+    end
+  end
+
+  def self.match_candlelight(codes)
+    codes.include?("CA")
+  end
+
+  def self.get_beginners
+    self.find_or_create_by(code: "B") do |format|
+      format.name = "Beginners"
+    end
+  end
+
+  def self.match_beginners(codes)
+    codes.include?("B") && (codes.count("B") != 2)
+  end
+
+  def self.get_formats(codes)
+    get_format_methods.each_with_object([]) do |method, results|
+      if self.send("match_#{method}".to_sym, codes)
+        results << self.send("get_#{method}".to_sym)
+      end
+    end
+  end
+
+  def self.get_format_methods
+    [:open, :closed, :speaker, :step, :big_book, :grapevine,
+      :traditions, :candlelight, :beginners]
+  end
+
+  def self.permitted_formats
+    { "O" => "Open",
+    "C" => "Closed",
+    "SP" => "Speaker",
+    "ST" => "Step",
+    "BB" => "Big Book",
+    "GV" => "Grapevine",
+    "T" => "Traditions",
+    "CA" => "Candlelight",
+    "B" => "Beginners"}
+  end
 end
--- spec/models/format_spec.rb
+++ spec/models/format_spec.rb
@@ -1,5 +1,130 @@
 require 'rails_helper'
 
 RSpec.describe Format, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include_context "codes"
+  let(:valid_formats) do
+    { "O" => "Open",
+    "C" => "Closed",
+    "SP" => "Speaker",
+    "ST" => "Step",
+    "BB" => "Big Book",
+    "GV" => "Grapevine",
+    "T" => "Traditions",
+    "CA" => "Candlelight",
+    "B" => "Beginners"}
+  end
+
+  it "can be created with a valid format" do
+    code_format = valid_formats.first
+    attributes = [:code, :name].zip(code_format).to_h
+
+    count = Format.count
+    Format.create(attributes)
+
+    expect(Format.count).to eq(count + 1)
+  end
+
+  it "allows only valid formats" do
+    bad_format = Format.new(code: "G", name: "German")
+
+    expect(bad_format).to_not be_valid
+  end
+
+  context "does not allow duplicate" do
+    before do
+      @code_format = valid_formats.first
+      @attributes = [:code, :name].zip(@code_format).to_h
+
+      Format.create(@attributes)
+    end
+
+    it "code" do
+      attributes = {code: @code_format.first,
+        name: valid_formats["C"].last}
+
+      dup = Format.new(attributes)
+
+      expect(dup).to_not be_valid
+    end
+
+    it "format" do
+      attributes = {code: valid_formats["C"].first,
+        name: @code_format.last}
+
+      dup = Format.new(attributes)
+
+      expect(dup).to_not be_valid
+    end
+  end
+
+  context "parses from aggregate code" do
+
+    it "finds O" do
+      raw_code = "O"
+      formats = Format.get_formats(raw_code)
+
+      expect(formats.count).to eq(1)
+      expect(formats.first.name).to eq("Open")
+    end
+
+    it "finds closed" do
+      raw_code = "*nC"
+      formats = Format.get_formats(raw_code)
+      formats = formats.map { |f| f.name }
+
+      expect(formats.count).to eq(1)
+      expect(formats.include?("Closed")).to be_truthy
+    end
+
+    it "finds grapevine" do
+      raw_code = "GV"
+      formats = Format.get_formats(raw_code)
+
+      expect(formats.first.name).to eq("Grapevine")
+    end
+
+    it "does not give closed false positive with CA" do
+      raw_code = "CA"
+      formats = Format.get_formats(raw_code)
+
+      expect(formats.first.name).to eq("Candlelight")
+      expect(formats.count).to eq(1)
+    end
+
+    it "does not give beginner false positive with BB" do
+      raw_code = "BB"
+      formats = Format.get_formats(raw_code)
+
+      expect(formats.count).to eq(1)
+      expect(formats.first.name).to eq("Big Book")
+    end
+
+    it "finds traditions" do
+      raw_code = "T"
+      formats = Format.get_formats(raw_code)
+
+      expect(formats.first.name).to eq("Traditions")
+    end
+
+    it "does not find traditions in ST" do
+      raw_code = "ST"
+      formats = Format.get_formats(raw_code)
+
+      expect(formats.count).to eq(1)
+      expect(formats.first.name).to eq("Step")
+    end
+
+    it "finds with other codes" do
+      raw_code = "BBBSTTC"
+      formats = Format.get_formats(raw_code)
+      formats = formats.map {|f| f.name }
+
+      expected_names = ["Big Book", "Beginners", "Step", "Traditions", "Closed"]
+
+      expected_names.each do |name|
+        expect(formats.include?(name)).to be_truthy
+      end
+    end
+
+  end
 end
