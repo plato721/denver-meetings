309160a9f14dda2a2f684bc87af5f27685611d9a
Matt Ewell <matt.ewell@gmail.com>
2015-11-17 13:54:56 -0700

    Distance sorting working.. if a user waits long enough to click search

--- app/models/search.rb
+++ app/models/search.rb
@@ -30,8 +30,14 @@ class Search < ActiveRecord::Base
   def results
     return HereAndNow.new(self.to_h).search if self.here_and_now
     raw_meetings = Meeting.search(self.to_h)
-    # add_distance(raw_meetings) if 
+    return distance_with(raw_meetings) if self.here 
     MobileListDisplay.new(raw_meetings)
+  end
+
+  def distance_with(meetings)
+    distance_finder = HereAndNow.new(self.to_h)
+    meeting_tuples = distance_finder.meetings_with_distance(meetings)
+    DistanceMeetings.new(meeting_tuples)
   end
 
   def to_h
