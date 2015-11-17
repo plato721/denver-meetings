04e9be16f302890ca3d157bb0b243051503728dc
Matt Ewell <matt.ewell@gmail.com>
2015-11-16 22:40:27 -0700

    Git rid of modules. Not sure how to make them work with class methods

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,6 +1,5 @@
 class Meeting < ActiveRecord::Base
-  include SearchFocus
-  
+
   geocoded_by :address, :latitude => :lat, :longitude => :lng
   after_validation :geocode
   before_create :address_from_coords
@@ -24,6 +23,7 @@ class Meeting < ActiveRecord::Base
     .by_gay(params[:gay])
     .by_men(params[:men])
     .by_women(params[:women])
+    .by_polish(params[:polish])
     .distinct
   end
 
@@ -69,7 +69,38 @@ class Meeting < ActiveRecord::Base
     where("address_1 LIKE ?", text)
   end
 
-  def self.by_gay(gay)
+  def address
+    [address_1, city, state].compact.join(', ')
+  end
+
+  def geocoder
+    geocoder ||= begin
+      if lat && lng
+        Geocoder.search([self.lat, self.lng])
+        .first.data["address_components"]
+      else
+        [][]
+      end
+    end
+  end
+
+  def calculated_zip
+    zip ||= geocoder.last["long_name"]
+  end
+
+  def calculated_street_number
+    num ||= geocoder[0]["long_name"]
+  end
+
+  def calculated_street_name
+    name ||= geocoder[1]["short_name"]
+  end
+
+  def address_from_coords
+    self.zip = calculated_zip
+  end
+
+    def self.by_gay(gay)
     return all if gay == "show"
     if gay == "only"
       includes(:foci).where(foci: { name: "Gay" })
@@ -105,34 +136,14 @@ class Meeting < ActiveRecord::Base
     end
   end
 
-  def address
-    [address_1, city, state].compact.join(', ')
-  end
-
-  def geocoder
-    geocoder ||= begin
-      if lat && lng
-        Geocoder.search([self.lat, self.lng])
-        .first.data["address_components"]
-      else
-        [][]
-      end
+  def self.by_polish(polish)
+    return all if polish == "show"
+    if polish == "only"
+      includes(:foci).where(foci: { name: "Polish" })
+    elsif polish == "hide"
+      includes(:foci).where(foci: { name: ["Spanish", "French", nil] })
     end
   end
 
-  def calculated_zip
-    zip ||= geocoder.last["long_name"]
-  end
 
-  def calculated_street_number
-    num ||= geocoder[0]["long_name"]
-  end
-
-  def calculated_street_name
-    name ||= geocoder[1]["short_name"]
-  end
-
-  def address_from_coords
-    self.zip = calculated_zip
-  end
 end
