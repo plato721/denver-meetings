3bb3e65faad0f1e5bb9aca8cd3d7563027025271
Matt Ewell <matt.ewell@gmail.com>
2015-11-16 19:32:22 -0700

    Adjust meeting test to account for corrected associations

--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Meeting, type: :model do
     create_languages
   end
 
-  it "does not allow duplicate formats" do
+  it "has formats" do
     meeting = Meeting.first
 
     format = Format.first
@@ -19,12 +19,9 @@ RSpec.describe Meeting, type: :model do
 
     meeting.formats << format
     expect(meeting.formats.count).to eq(1)
-
-    meeting.formats << format
-    expect(meeting.formats.count).to eq(1)
   end
 
-  it "does not allow duplicate features" do
+  it "has features" do
     meeting = Meeting.first
 
     feature = Feature.first
@@ -32,12 +29,9 @@ RSpec.describe Meeting, type: :model do
 
     meeting.features << feature
     expect(meeting.features.count).to eq(1)
-
-    meeting.features << feature
-    expect(meeting.features.count).to eq(1)
   end
 
-  it "does not allow duplicate foci" do
+  it "has foci" do
     meeting = Meeting.first
 
     focus = Focus.first
@@ -45,19 +39,13 @@ RSpec.describe Meeting, type: :model do
 
     meeting.foci << focus
     expect(meeting.foci.count).to eq(1)
-
-    meeting.foci << focus
-    expect(meeting.foci.count).to eq(1)
   end
 
-  it "does not allow duplicate languages" do
+  it "has languages" do
     meeting = Meeting.first
 
     language = Language.first
     expect(meeting.languages.empty?).to be_truthy
-
-    meeting.languages << language
-    expect(meeting.languages.count).to eq(1)
 
     meeting.languages << language
     expect(meeting.languages.count).to eq(1)
