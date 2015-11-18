fb7496cfe2cd7bb3a8b590e6b724ca0c59f0f35b
Matt Ewell <matt.ewell@gmail.com>
2015-11-18 09:58:23 -0700

    Refactor. Create raw_meetings method used by both distance and weekday entry points

--- app/models/search.rb
+++ app/models/search.rb
@@ -5,12 +5,10 @@ class Search < ActiveRecord::Base
 
   def distance_results
     return HereAndNow.new(self.to_h).search if self.here_and_now
-    raw_meetings = Meeting.search(self.to_h)
     distance_with(raw_meetings)
   end
 
   def weekday_results
-    raw_meetings = Meeting.search(self.to_h)
     WeekdayMeetings.new(raw_meetings)
   end
 
@@ -18,6 +16,10 @@ class Search < ActiveRecord::Base
     distance_finder = HereAndNow.new(self.to_h)
     meeting_tuples = distance_finder.meetings_with_distance(meetings)
     DistanceMeetings.new(meeting_tuples)
+  end
+
+  def raw_meetings
+    Meeting.search(self.to_h)
   end
 
   def to_h
