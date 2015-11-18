103f491d5efb96445b7177c0f43b647b47f72494
Matt Ewell <matt.ewell@gmail.com>
2015-11-18 07:04:22 -0700

    Set up caching for index thumbnails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def cache_key_for(model_class, label = "")
+    prefix = model_class.to_s.downcase.pluralize
+    count = model_class.count
+    last_updated = model_class.maximum(:updated_at)
+
+    [prefix, count, last_updated].join("-")
+  end
 end
