d72e9294e96e0949c30809e0355d740f2f888903
Matt Ewell <matt.ewell@gmail.com>
2015-11-17 21:53:11 -0700

    Remove two dead methods from search

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,32 +1,4 @@
 class Search < ActiveRecord::Base
-
-  # def self.create_search(params)
-  #   self.create({free: params["free_search"],
-  #             city: params["city"],
-  #             group_name: params["name"],
-  #             day: params["day"],
-  #             time: params["time"],
-  #             open: params["open"],
-  #             lat: params["lat"],
-  #             lng: params["lng"],
-  #             here_and_now: params["here_and_now"],
-  #             here: params["here"],
-  #             # city_text: params["city_text"],
-  #             # group_text: params["group_text"],
-  #             open: params["open"],
-  #             men: params["men"],
-  #             women: params["women"],
-  #             youth: params["youth"],
-  #             gay: params["gay"],
-  #             access: params["access"],
-  #             non_smoking: params["non_smoking"],
-  #             sitter: params["sitter"],
-  #             spanish: params["spanish"],
-  #             polish: params["polish"],
-  #             french: params["french"]
-  #             })
-  # end
-
   def results
     return HereAndNow.new(self.to_h).search if self.here_and_now
     raw_meetings = Meeting.search(self.to_h)
@@ -46,13 +18,4 @@ class Search < ActiveRecord::Base
     end.to_h
   end
 
-  def remove_empty_params
-    all = search_to_params_values
-  end
-
-  def search_to_params_values
-    Search.columns.map do |param|
-      [param, self.send(param.to_sym)]
-    end
-  end
 end
