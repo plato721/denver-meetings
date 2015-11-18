5fe92e336b555767a918d3936ecae63acc143eda
Matt Ewell <matt.ewell@gmail.com>
2015-11-18 11:28:54 -0700

    Test admin meeting edit

--- spec/features/admin_meeting_display_spec.rb
+++ spec/features/admin_meeting_display_spec.rb
@@ -7,6 +7,8 @@ RSpec.feature "Admin meeting display" do
     let(:admin){ User.find_by(nickname: "plato721") }
     before do
       login(admin)
+      WebMock.stub_request(:get, "http://maps.googleapis.com/maps/api/geocode/json?address=3355%20S.%20Wadsworth%20Ave.,%20Lakewood,%20CO&language=en&sensor=false")
+      allow_any_instance_of(Meeting).to receive(:calculated_zip).and_return("12345")
     end
 
     it "displays a list of meetings" do
@@ -16,12 +18,19 @@ RSpec.feature "Admin meeting display" do
     end
 
     it "can edit a meeting" do
+      meeting = Meeting.first
+      id = meeting.id
+      group_name = meeting.group_name
+
       visit admin_meetings_path
-      within(:css, "td#approval-display-#{Meeting.first.id}") do
+      within(:css, "td#approval-display-#{id}") do
         click_link_or_button "False"
       end
-      expect(current_path).to eq("/admin/meetings/#{Meeting.first.id}/edit")
-      
+      expect(current_path).to eq("/admin/meetings/#{id}/edit")
+      fill_in "meeting_group_name", with: "Patterns on whiteboards"
+      click_link_or_button "Update Meeting"
+      expect(page).to have_content("Patterns on whiteboards")
+      expect(page).to_not have_content(group_name)
     end
   end
 end
