1190fae2736b03e7671698bcc3b65d8e0099de79
Matt Ewell <matt.ewell@gmail.com>
2015-11-18 17:14:57 -0700

    Add here proximity test (separate from here and now)

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 RSpec.describe Search do
   fixtures :meetings
 
-  before(:each) do
-    @search_params = {
-      city: "Any",
-      group_name: "Any",
-      day: "Any",
-      time: "Any"
-    }
+  it "finds by location" do
+    search = Search.new({lat: 39.740143,
+                         lng: -104.962335,
+                         here: true})
+
+    closest_meeting = search.results.first.last.first
+    # (first group, associated meetings, first of the associated meetings)
+    expect(closest_meeting.address.include?("1311 York")).to be_truthy
   end
 
   xit "finds by name" do
