2f33b1ae6b0ff0f35600cb7959e1a3e0e118738e
Matt Ewell <matt.ewell@gmail.com>
2016-04-03 14:05:51 -0600

    Factor out scraping from rake task to poro
    Also:
     - Add description for rake task so it shows up in
    rake -T
     - Add force flag - will not compare updated dates if used
     - Add local vs. remote update checking to see if update
    needed

--- lib/tasks/scrape.rake
+++ lib/tasks/scrape.rake
@@ -1,61 +1,7 @@
-def url
-  url = "http://www.daccaa.org/query.asp"
-end
-
-def set_updated(data)
-  updated = last_updated(data)
-  RawMeetingMetadata.create(last_update: updated)
-end
-
-def last_updated(data)
-  raw = data.find { |e| e.include? "Database Last Updated" }
-  raw = raw.slice(22..-1)
-  DateTime.strptime(raw, "%m/%d/%Y %I:%M:%S %p")
-end
-
-def update_meetings(data)
-  trimmed = data.slice(13..-68)
-  trimmed.each_slice(7) do |slice|
-    RawMeeting.add_from(slice)
-  end
-end
-
-def get_meetings_raw
-  RestClient.post(url, {
-  'txtGroup'=>'',
-  'cboDay'=>'0',
-  'cboStartTime'=>'All',
-  'cboEndTime'=>'All',
-  'txtCity'=>'',
-  'cboMeetingType'=>'All',
-  'cboMeetingFormat'=>'All',
-  'cboSpecialMeeting'=>'All',
-  'cboDistrict'=>'All',
-  'cmdFindMeetings'=>'Find Meetings'
-  })
-end
-
-def parse_meetings(page)
-    npage = Nokogiri::HTML(page)
-    data = npage.search('//text()').map(&:text).delete_if{|x| x !~ /\w|\*/}
-    data = data.map {|ugly| ugly.lstrip} #remove leading whitespace
-end
-
-def create_displayable
-  RawMeeting.all.each do |raw|
-    if !Meeting.joins(:raw_meeting).exists?(raw.id)
-      sleep 1.0
-      MeetingCreator.new(raw).create
-    end
-  end
-end
-
 namespace :daccaa do
-  task :get_data => :environment do
-    page = get_meetings_raw
-    data = parse_meetings(page)
-    set_updated(data)
-    update_meetings(data)
-    create_displayable
+  desc "scrape daccaa for meetings"
+  task :get_data, [:force] => :environment do |t, args|
+    force = args[:force]
+    scraper = ScrapeDaccaa.scrape(force)
   end
 end
---
+++ spec/models/scrape_daccaa_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe ScrapeDaccaa do
+  before(:all) do
+    @scraper = ScrapeDaccaa.new
+  end
+
+  def valid?(url)
+    !!URI.parse(url)
+    rescue URI::InvalidURIError
+      false
+  end
+
+  it "has a url" do
+    scraper = ScrapeDaccaa.new
+
+    expect(valid? scraper.url).to be_truthy
+  end
+
+  context "meeting update decision logic" do
+    before(:all) do
+      @now = DateTime.now
+      @yesterday = @now.yesterday
+      @tomorrow = @now.tomorrow
+      dates = [@now, @yesterday, @tomorrow].shuffle.shuffle
+
+      dates.each do |date|
+        RawMeetingMetadata.create(last_update: date)
+      end
+    end
+
+    it "knows when local (denver meetings) was updated" do
+      expect(@scraper.local_updated).to eq(@tomorrow)
+    end
+
+    it "wants to update when local updated older than remote" do
+      allow_any_instance_of(ScrapeDaccaa).to receive(:local_updated)
+        .and_return(@yesterday)
+      allow_any_instance_of(ScrapeDaccaa).to receive(:remote_updated)
+        .and_return(@tomorrow)
+
+      expect(@scraper.update_needed?).to be_truthy
+    end
+
+    it "wants to update when forced" do
+      scrape = ScrapeDaccaa.new(true)
+      unforced_scrape = ScrapeDaccaa.new
+
+      allow_any_instance_of(ScrapeDaccaa).to receive(:local_updated)
+        .and_return(@now)
+      allow_any_instance_of(ScrapeDaccaa).to receive(:remote_updated)
+        .and_return(@yesterday)
+
+      expect(unforced_scrape.update_needed?).to be_falsey
+      expect(scrape.update_needed?).to be_truthy
+    end
+
+    it "won't update if the updates weigh the same" do
+      allow_any_instance_of(ScrapeDaccaa).to receive(:local_updated)
+        .and_return(@now)
+      allow_any_instance_of(ScrapeDaccaa).to receive(:remote_updated)
+        .and_return(@now)
+
+      expect(@scraper.update_needed?).to be_falsey
+    end
+  end
+end
