e9992810941b04bd7b56da5ce007fedd3224eda7
Matt Ewell <matt.ewell@gmail.com>
2016-04-13 22:04:37 -0600

    remove arguments, make runner an instance method
    can add arguments back in if there's a use case. otherwise,
    you create a new meetings_creator with new raw)meetings,
    execute the runner or the other methods as you see fit.
    if you run the runner, you are left with ingredients for
    a report in deleted, untouched, and created

--- app/models/meetings_creator.rb
+++ app/models/meetings_creator.rb
@@ -8,33 +8,41 @@ class MeetingsCreator
 
   def self.update_displayable(raw_meetings)
     creator = new(raw_meetings)
-    # visible meetings, no retrieved/created raw_meeting
-    deleted = creator.perform_soft_delete(raw_meetings)
-    created = creator.create_displayable(raw_meetings)
-    creator
+    creator.tap { |c| c.update_displayable }
   end
 
-  def perform_soft_delete(raw = self.raw_meetings)
-    to_be_deleted(raw).each do |meeting|
+  def update_displayable
+    self.deleted = creator.perform_soft_delete
+    self.untouched = visible_meetings # must delete first
+    self.created = creator.create_displayable
+  end
+
+  def perform_soft_delete
+    to_be_deleted.each do |meeting|
+      # hide em, set them to "pending deletion"
       meeting.update_attribute(:deleted, true)
-      meeting.update_attribute(:visible, false)# hide em, set them to "pending deletion"
+      meeting.update_attribute(:visible, false)
     end
   end
 
-  def to_be_deleted(raw = self.raw_meetings)
-    bad_raw = RawMeeting.for_all_visible_meetings - raw
+  def to_be_deleted
+    bad_raw = RawMeeting.for_all_visible_meetings - self.raw_meetings
     displayables_to_hide = Meeting.where(raw_meeting_id: bad_raw)
     # select admin override (have some way to protect meetings
         # manually selected)
   end
 
-  def to_be_created(raw = self.raw_meetings)
-    have = Meeting.where(visible: true).pluck(:raw_meeting_id)
-    need = raw.select { |raw_meeting| !have.include? raw_meeting.id }
+  def visible_meetings
+    Meeting.where(visible: true)
   end
 
-  def create_displayable(raw = self.raw_meetings)
-    self.created = to_be_created(raw).map do |raw|
+  def to_be_created
+    have = visible_meetings.pluck(:raw_meeting_id)
+    need = raw_meetings.select { |raw_meeting| !have.include? raw_meeting.id }
+  end
+
+  def create_displayable
+    to_be_created.map do |raw|
       sleep 1.0 #geocoder
       MeetingCreator.new(raw).create
     end
