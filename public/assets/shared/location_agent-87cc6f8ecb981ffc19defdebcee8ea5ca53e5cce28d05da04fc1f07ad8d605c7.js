24724eab677d1dd355a849109e19b4a86e60a3d8
Matt Ewell <matt.ewell@gmail.com>
2016-10-02 09:40:13 -0600

    precompile assets

---
+++ public/assets/shared/location_agent-87cc6f8ecb981ffc19defdebcee8ea5ca53e5cce28d05da04fc1f07ad8d605c7.js
@@ -0,0 +1,36 @@
+function checkLocation(cb){
+  if( !isLocationSearch() ){ return cb(); }
+
+  var searchFor = searchBox().val();
+  cleanAndSearch(searchFor, cb);
+}
+
+function googleApiUrl(){
+  return "https://maps.googleapis.com/maps/api/geocode/json";
+}
+
+// Todo: clean and search, not just search :-)
+function cleanAndSearch(term, cb){
+  $.ajax({
+    url: googleApiUrl(),
+    data: { address : term,
+            key : googleMapsKey() },
+    success: storeLocation
+  })
+}
+
+function storeLocation(data){
+  if( data.status === "OK" ){
+    $('#lat').val(data.results[0].geometry.location.lat);
+    $('#lng').val(data.results[0].geometry.location.lng);
+  }
+  submitForm();
+}
+
+function isLocationSearch(){
+  return ( locationBox().val() === 'true' )
+}
+
+$(document).ready(function(){
+  //console.log("location_agent loaded");
+}());
