2db63d54f86ce2e3a7fa40aaca3cc823b23b72c1
plato721 <matt.ewell@gmail.com>
2019-02-12 13:41:22 -0700

    Remove bad references to now-namespaced extract_phone

--- app/models/meeting/extract_phone.rb
+++ app/models/meeting/extract_phone.rb
@@ -2,36 +2,34 @@
 # addresses. That is, it gets the phone number, saves it where it should be,
 # removes the number and any leading comma from the address, and stores the
 # address.
-module Meeting
-  class ExtractPhone
-    class << self
-      def extract! meeting
-        initial_address = meeting.address_1
-        return unless has_phone? initial_address
+class Meeting::ExtractPhone
+  class << self
+    def extract! meeting
+      initial_address = meeting.address_1
+      return unless has_phone? initial_address
 
-        phone_part = initial_address.match(phone_regex).to_s
-        address_part = scrub_phone_from(initial_address)
+      phone_part = initial_address.match(phone_regex).to_s
+      address_part = scrub_phone_from(initial_address)
 
-        meeting.update_column(:address_1, address_part)
-        meeting.update_column(:phone, phone_part)
-      end
+      meeting.update_column(:address_1, address_part)
+      meeting.update_column(:phone, phone_part)
+    end
 
-      def has_phone? address
-        address.match phone_regex
-      end
+    def has_phone? address
+      address.match phone_regex
+    end
 
-      def phone_regex
-        MeetingCreator::PhoneExtractor.phone_matcher
-      end
+    def phone_regex
+      MeetingCreator::PhoneExtractor.phone_matcher
+    end
 
-      def phone_and_leading_comma_regex
-        /(, )?#{phone_regex}/
-      end
+    def phone_and_leading_comma_regex
+      /(, )?#{phone_regex}/
+    end
 
-      def scrub_phone_from address
-        phone_and_trimmings = address.match(phone_and_leading_comma_regex).to_s
-        address.gsub(phone_and_trimmings, "")
-      end
+    def scrub_phone_from address
+      phone_and_trimmings = address.match(phone_and_leading_comma_regex).to_s
+      address.gsub(phone_and_trimmings, "")
     end
   end
 end
--- lib/tasks/scrub.rake
+++ lib/tasks/scrub.rake
@@ -1,5 +1,3 @@
-require Rails.root.join('./app/models/meeting/extract_phone.rb').to_s
-
 namespace :scrub do
   desc 'extract and store phone number'
   task phone: :environment do
