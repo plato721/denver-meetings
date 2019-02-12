d1f51d7bcd5578d7fd460a86b3a3cc2e4a07dc9e
plato721 <matt.ewell@gmail.com>
2019-02-12 09:48:18 -0700

    Complete address2_extractor
    This will ignore address2's that are in parentheses.. But
    those will just appear as notes anyway. Spending way
    too much time on this as it is.

--- app/models/meeting_creator/address2_extractor.rb
+++ app/models/meeting_creator/address2_extractor.rb
@@ -1,8 +1,17 @@
 class MeetingCreator::Address2Extractor
   def self.extract(raw_meeting)
-    matched = raw_meeting.address.match /(Unit|Ste|#)[ ]{0,}[\w-]+/
+    address = remove_notes(raw_meeting.address)
+    matched = address.match /(Unit|Ste|#)[\.]?[ ]{0,}[\w-]+/
     return '' unless matched
 
     matched.to_s
+  end
+
+  def self.remove_notes(address)
+    matcher = MeetingCreator::NotesExtractor.notes_matcher
+    matched = address.match(matcher)
+    return address unless matched
+
+    address.delete(matched.to_s)
   end
 end
--- app/models/meeting_creator/notes_extractor.rb
+++ app/models/meeting_creator/notes_extractor.rb
@@ -1,7 +1,11 @@
 class MeetingCreator::NotesExtractor
   def self.extract(raw_meeting)
-    notes_match = raw_meeting.address.match(/\(.+\)/)
+    notes_match = raw_meeting.address.match(notes_matcher)
     notes = notes_match ? notes_match.to_s : ""
     notes.delete(')(')
+  end
+
+  def self.notes_matcher
+    /\(.+\)/
   end
 end
