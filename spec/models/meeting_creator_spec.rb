64464f50e922789f3531b64aaaed154f70f13ef3
plato721 <matt.ewell@gmail.com>
2019-02-12 10:10:09 -0700

    Uncomment and tweak rest of meeting creator specs

--- spec/models/meeting_creator_spec.rb
+++ spec/models/meeting_creator_spec.rb
@@ -85,51 +85,51 @@ RSpec.describe MeetingCreator do
     end
   end
 
-  # context "with another phone number" do
-  #   before :all do
-  #     @raw = RawMeeting.create(
-  #       group_name: "Buckeye Easy Does It",
-  #       address: "16732 E Iliff Av (Shop Ctr) 695-7766"
-  #     )
-  #     @mc = MeetingCreator.new(@raw)
-  #   end
+  context "with another phone number" do
+    before :all do
+      @raw = RawMeeting.create(
+        group_name: "Buckeye Easy Does It",
+        address: "16732 E Iliff Av (Shop Ctr) 695-7766"
+      )
+      @meeting = MeetingCreator.new(@raw).create
+    end
 
-  #   it "parses address_1" do
-  #     expect(@mc.address_1).to eql("16732 E Iliff Av")
-  #   end
+    it "parses address_1" do
+      expect(@meeting.address_1).to eql("16732 E Iliff Av")
+    end
 
-  #   it "parses notes" do
-  #     expect(@mc.notes).to eql("Shop Ctr")
-  #   end
+    it "parses notes" do
+      expect(@meeting.notes).to eql("Shop Ctr")
+    end
 
-  #   it "parses phone number" do
-  #     expect(@mc.phone).to eql("695-7766")
-  #   end
-  # end
+    it "parses phone number" do
+      expect(@meeting.phone).to eql("695-7766")
+    end
+  end
 
-  # context "another phone and address pattern" do
-  #   before :all do
-  #     @raw = RawMeeting.create(
-  #       group_name: "231 Buckley",
-  #       address: "15210 E 6th Ave, Unit 1, 343-4994"
-  #     )
-  #     @mc = MeetingCreator.new(@raw)
-  #   end
+  context "another phone and address pattern" do
+    before :all do
+      @raw = RawMeeting.create(
+        group_name: "231 Buckley",
+        address: "15210 E 6th Ave, Unit 1, 343-4994"
+      )
+      @meeting = MeetingCreator.new(@raw).create
+    end
 
-  #   it "parses address_1" do
-  #     expect(@mc.address_1).to eql("15210 E 6th Ave")
-  #   end
+    it "parses address_1" do
+      expect(@meeting.address_1).to eql("15210 E 6th Ave")
+    end
 
-  #   it "parses address_2" do
-  #     expect(@mc.address_2).to eql("Unit 1")
-  #   end
+    it "parses address_2" do
+      expect(@meeting.address_2).to eql("Unit 1")
+    end
 
-  #   it "parses notes" do
-  #     expect(@mc.notes).to eql("")
-  #   end
+    it "parses notes" do
+      expect(@meeting.notes).to eql("")
+    end
 
-  #   it "parses phone number" do
-  #     expect(@mc.phone).to eql("343-4994")
-  #   end
-  # end
+    it "parses phone number" do
+      expect(@meeting.phone).to eql("343-4994")
+    end
+  end
 end
